4f2a4c0a0b1524a4ac9bf34cce872c5cd1746548
Germain Soumier <germain.soumier@gmail.com>
2012-03-07 20:05:02 +0100

    First commit of repo WhereIsIt

---
+++ src/fr/arolla/afterwork/MainActivity.java
@@ -0,0 +1,27 @@
+package fr.arolla.afterwork;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button button = (Button) findViewById(R.id.newGame);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("WhereIsIt");
+				startActivity(intent);
+			}
+		});
+	}
+
+}
---
+++ src/fr/arolla/afterwork/WhereIsItActivity.java
@@ -0,0 +1,13 @@
+package fr.arolla.afterwork;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhereIsItActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.where_is_it);
+	}
+}
