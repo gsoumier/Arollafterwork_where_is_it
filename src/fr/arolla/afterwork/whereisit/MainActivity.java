a99551147c6a3c8c406a3f3b228c09f95c665aec
Germain Soumier <germain.soumier@gmail.com>
2012-05-08 21:32:43 +0200

    ajout des libs d'un tutoriel

--- src/fr/arolla/afterwork/whereisit/MainActivity.java
+++ src/fr/arolla/afterwork/whereisit/MainActivity.java
@@ -1,8 +1,6 @@
 package fr.arolla.afterwork.whereisit;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Iterator;
@@ -10,17 +8,17 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
+import com.google.api.services.picasa.model.AlbumFeed;
+import com.google.api.services.picasa.model.PhotoEntry;
 import com.google.gdata.client.photos.PicasawebService;
-import com.google.gdata.data.photos.AlbumFeed;
-import com.google.gdata.data.photos.PhotoEntry;
 
+import fr.arolla.afterwork.whereisit.MainActivity.PhotoOnClickListener;
 import fr.arolla.afterwork.whereisit.db.WhereIsItDBAdapter;
 
 public class MainActivity extends Activity {
@@ -29,7 +27,6 @@ public class MainActivity extends Activity {
 
 	String albumUrl = "https://picasaweb.google.com/data/feed/api/user/germain.soumier/albumid/5734132424019816081";
 
-	
 	private Iterator<PhotoEntry> photoIterator;
 
 	private ImageView imageView;
@@ -42,11 +39,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.main);
 
 		PicasawebService myService = new PicasawebService("Picasa-WhereIsIt");
-		// try {
-		// myService.setUserCredentials("liz@gmail.com", "mypassword");
-		// } catch (AuthenticationException e) {
-		// e.printStackTrace();
-		// }
+
 		try {
 			URL feedUrl = new URL(albumUrl);
 			AlbumFeed feed = myService.getFeed(feedUrl, AlbumFeed.class);
@@ -59,45 +52,44 @@ public class MainActivity extends Activity {
 		imageView = (ImageView) findViewById(R.id.image);
 		photoOnClickListener = new PhotoOnClickListener();
 		imageView.setOnClickListener(photoOnClickListener);
-		
-		if(photoIterator.hasNext()){
+
+		if (photoIterator.hasNext()) {
 			PhotoEntry photoEntry = photoIterator.next();
 			updateWithNextPhoto(photoEntry);
 		}
-		
-		
+
 	}
 
 	private void updateWithNextPhoto(PhotoEntry photoEntry) {
 		photoOnClickListener.setEntry(photoEntry);
 		String photoUrl = photoEntry.getMediaThumbnails().get(0).getUrl();
-		Drawable photoDrawable= null;
+		Drawable photoDrawable = null;
 		try {
 			URL urlO = new URL(photoUrl);
-			URLConnection urlC = urlO.openConnection(); 
-			InputStream is = urlC.getInputStream(); 
+			URLConnection urlC = urlO.openConnection();
+			InputStream is = urlC.getInputStream();
 			photoDrawable = Drawable.createFromStream(is, photoUrl);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 		imageView.setImageDrawable(photoDrawable);
 	}
-	
-	class PhotoOnClickListener implements OnClickListener{
-		
+
+	class PhotoOnClickListener implements OnClickListener {
+
 		PhotoEntry entry;
-		
+
 		public void setEntry(PhotoEntry entry) {
 			this.entry = entry;
 		}
 
 		public void onClick(View v) {
 			Intent intent = new Intent("WhereIsIt");
-			if(entry==null)
+			if (entry == null)
 				return;
 			intent.putExtra("lat", entry.getGeoLocation().getLatitude());
 			startActivity(intent);
 		}
-		
+
 	}
 }
